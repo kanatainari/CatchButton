d9c1a6c0142012d959d41d6f64e31185875046be
강민영 <kt24643269@gmail.com>
2026-03-12 16:20:20 +0900

    초기 커밋.

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+namespace CatchButton
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            Random rd = new Random();
+
+            int maxX = this.ClientSize.Width - Button_true.Width;
+            int maxY = this.ClientSize.Height - Button_true.Height;
+
+            int nextX = rd.Next(0, maxX);
+            int nextY = rd.Next(0, maxY);
+
+            Button_true.Location = new Point(nextX, nextY);
+
+            this.Text = $"(버튼 위치 : {nextX}, {nextY})";
+        }
+    }
+}
